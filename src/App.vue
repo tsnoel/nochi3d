6a46df38509f65eb14f6b3bfe936139895614105
Tim Noel <tnoel@eitccorp.com>
2022-05-07 02:29:36 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <demo/>
+</template>
+
+<script>
+import Demo from './components/Demo'
+
+export default {
+    name: 'App',
+    components: {
+        [Demo.name]: Demo
+    }
+}
+</script>
+
+<style>
+html,
+body {
+    height: 100%;
+    width: 100%;
+}
+
+html {
+    overflow: hidden;
+}
+
+body {
+    margin: 0;
+}
+
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+    width: 100%;
+}
+</style>
