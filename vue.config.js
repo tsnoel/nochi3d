e038768baed8439a7ac3862deff401e52c97178d
Tim Noel <tnoel@eitccorp.com>
2022-05-07 11:05:36 -0400

    Build with new vue config

--- vue.config.js
+++ vue.config.js
@@ -20,7 +20,7 @@ module.exports = {
         output: {
             chunkFilename: '[name].js?id=[chunkhash]',
             filename: '[name].js?id=[hash]',
-            globalObject: 'owo'
+            globalObject: 'window'
         },
         resolve: {
             extensions: ['.js', '.vue', '.json'],
